4002b5a485a07bcc4a01fe5cd0fe87164d708915
Antony <ant.thiery@free.fr>
2025-03-24 11:47:58 +0100

    led et switch en meme temps

--- Reg8.cpp
+++ Reg8.cpp
@@ -8,7 +8,7 @@ Reg8::Reg8(MX3board * temp_board, unsigned char temp_addr) {
 
 
 void Reg8::operator=(unsigned char data){
-    board->board_d_write(addr_led, data);
+    board->board_d_write(MX3ADDR_LED, data);
 }
 
 Reg8::operator unsigned char(){
